dcf9bafbd66900f07796036e543811f23f8fed53
viktor <v.scherbyna@tinqin.com>
2025-03-07 10:15:27 +0200

    initial project setup

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "ottu_checkout_sdk",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "ottu_checkout_sdk",
+            targets: ["ottu_checkout_sdk", "ottu_checkout_sdk_dep"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.43.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ottu_checkout_sdk",
+            path: "./Sources/ottu_checkout_sdk.xcframework"
+        ),
+        .target(
+            name: "ottu_checkout_sdk_dep",
+            dependencies: [
+                         .product(name: "Sentry", package: "sentry-cocoa")
+                     ]
+        )
+    ]
+)
